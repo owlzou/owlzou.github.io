c6bace6a4f0580b8af2d5e3e7e0e05cfab6037ea
owlzou <38106927+owlzou@users.noreply.github.com>
2021-01-11 13:59:21 +0000

    Update from https://github.com/owlzou/blog-source/commit/4f78904b28a34f3be91c285a4e2257c884a89c37

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 //ICONS 目前在<script>标签的onload里执行
 //feather.replace();
 
+var is_menu_open = false;
+var top_fixed = false;
+
 //Simple lazy load
 document.querySelectorAll(".lazyload").forEach((img) => {
   img.src = img.attributes["data-src"].value;
 });
 
 //Menu
+const $moblie_header = document.querySelector(".moblie-header");
 document.querySelector(".burger").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.add("active");
-  document.querySelector(".mask").classList.add("active");
+  if (is_menu_open) {
+    document.querySelector(".sidebar").classList.remove("active");
+    document.querySelector(".burger").innerHTML = `<i data-feather="menu"></i>`;
+    $moblie_header.classList.remove("no-shadow");
+  } else {
+    document.querySelector(".sidebar").classList.add("active");
+    document.querySelector(".burger").innerHTML = `<i data-feather="x"></i>`;
+    $moblie_header.classList.add("no-shadow");
+  }
+  feather.replace();
+  is_menu_open = !is_menu_open;
 });
 
-document.querySelector(".mask").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.remove("active");
-  document.querySelector(".mask").classList.remove("active");
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 40 && !top_fixed) {
+    $moblie_header.classList.add("fixed");
+    top_fixed = true;
+  }
+  if (window.pageYOffset <= 40 && top_fixed) {
+    $moblie_header.classList.remove("fixed");
+    top_fixed = false;
+  }
 });
 
 //Scroll To Top
@@ -24,21 +43,56 @@ document.querySelector(".fab").addEventListener("click", () => {
     top: 0,
     behavior: "smooth",
   });
+  document.querySelector(".fab").blur();
 });
 
-//TOCBOT
-function toc_onload() {
-  tocbot.init({
-    tocSelector: ".toc",
-    // Where to grab the headings to build the table of contents.
-    contentSelector: ".main",
-    // Which headings to grab inside of the contentSelector element.
-    headingSelector: "h2, h3",
-    // For headings inside relative or absolute positioned containers within content.
-    hasInnerContainers: true,
+// TOC
+
+if (document.querySelector(".toc") != null) {
+  var toc = {};
+  //const title_height = 35;
+  var keys = {};
+
+  function getOffset(element) {
+    if (element.offsetParent) {
+      return element.offsetTop + getOffset(element.offsetParent);
+    } else {
+      return element.offsetTop;
+    }
+  }
+
+  function getCurId(keys) {
+    if (window.pageYOffset < toc[keys[0]]) {
+      return keys[0];
+    } else if (window.pageYOffset >= toc[keys[keys.length - 1]]) {
+      return keys[keys.length - 1];
+    } else {
+      for (let i = 0; i < keys.length - 1; i++) {
+        if (
+          window.pageYOffset >= toc[keys[i]] &&
+          window.pageYOffset < toc[keys[i + 1]]
+        ) {
+          return keys[i];
+        }
+      }
+    }
+  }
+
+  document.querySelectorAll("h2,h3").forEach((ele) => {
+    toc[`to-${ele.id}`] = getOffset(ele);
   });
-  $toc = document.querySelector(".toc");
-  if ($toc && $toc.children.length == 0) {
-    $toc.remove();
+
+  keys = Object.keys(toc);
+
+  if (keys.length > 0) {
+    window.addEventListener("scroll", function () {
+      let id = getCurId(keys);
+      keys.forEach((ele) =>
+        document.getElementById(ele).classList.remove("active")
+      );
+      document.getElementById(id).classList.add("active");
+    });
+  }else{
+    document.querySelector(".toc").remove();
   }
 }
